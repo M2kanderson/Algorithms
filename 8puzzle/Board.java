5673030a3b59cda71054f9dc3f1c6578e78d6f80
Justin Anderson <janderson2k@gmail.com>
2016-10-24 19:36:03 -0700

    added solver class to 8puzzle

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -1,13 +1,22 @@
 // import java.lang.Math.abs;
 import java.util.Stack;
+import java.util.Comparator;
+import edu.princeton.cs.algs4.MinPQ;
 
 public class Board {
    private int[][] tiles;
    private int moves;
+   public static final Comparator<Board> BY_HAMMING = new ByHamming();
+   public static final Comparator<Board> BY_MANHATTAN = new ByManhattan();
+
    public Board(int[][] tiles)        // construct a board from an N-by-N array of tiles
    {
      this.tiles = tiles;
      this.moves = 0;
+   }
+   public int tileAt(int row, int col)
+   {
+       return this.tiles[row][col];
    }
    public int hamming()               // return number of blocks out of place
    {
@@ -74,7 +83,7 @@ public class Board {
                newTiles[i][j] = this.tiles[i][j];
        }
        return new Board(newTiles);
-           
+
    }
    public Iterable<Board> neighbors() // return an Iterable of all neighboring board positions
    {
@@ -100,14 +109,16 @@ public class Board {
        int temp = newBoard.tiles[zeroX - 1][zeroY];
        newBoard.tiles[zeroX - 1][zeroY] = 0;
        newBoard.tiles[zeroX][zeroY] = temp;
+       newBoard.moves = this.moves + 1;
        stack.push(newBoard);
      }
-     if(zeroX < this.tiles.length)
+     if(zeroX < this.tiles.length - 1)
      {
        Board newBoard = dupBoard();
        int temp = newBoard.tiles[zeroX + 1][zeroY];
        newBoard.tiles[zeroX + 1][zeroY] = 0;
        newBoard.tiles[zeroX][zeroY] = temp;
+       newBoard.moves = this.moves + 1;
        stack.push(newBoard);
      }
      if(zeroY > 0){
@@ -115,14 +126,16 @@ public class Board {
        int temp = newBoard.tiles[zeroX][zeroY - 1];
        newBoard.tiles[zeroX][zeroY - 1] = 0;
        newBoard.tiles[zeroX][zeroY] = temp;
+       newBoard.moves = this.moves + 1;
        stack.push(newBoard);
      }
-     if(zeroY < this.tiles.length)
+     if(zeroY < this.tiles.length - 1)
      {
        Board newBoard = dupBoard();
        int temp = newBoard.tiles[zeroX][zeroY + 1];
        newBoard.tiles[zeroX][zeroY + 1] = 0;
        newBoard.tiles[zeroX][zeroY] = temp;
+       newBoard.moves = this.moves + 1;
        stack.push(newBoard);
      }
      return stack;
@@ -130,7 +143,6 @@ public class Board {
    }
    public String toString()           // return a string representation of the board
    {
-     System.out.println(this.tiles[0][2]);
      int N = this.tiles.length;
      String finalString = "";
      for(int i = 0; i < N; i++)
@@ -138,41 +150,115 @@ public class Board {
        String line = "";
        for(int j = 0; j < N; j++)
        {
-         line += this.tiles[i][j] + " ";
+         if(this.tiles[i][j] != 0)
+         {
+             line += this.tiles[i][j] + " ";
+         }
+         else
+         {
+             line += "  ";
+         }
+
        }
        finalString += line + "\n";
      }
-     return finalString;
+     return finalString + "\n";
+   }
+   
+   public Board altBoard()
+   {
+       int N = this.tiles.length;
+       int[][] newTiles = new int[N][N];
+       for(int i = 0; i < N; i++)
+       {
+           for(int j = 0; j < N; j++)
+               newTiles[i][j] = this.tiles[i][j];
+       }
+       int temp = newTiles[0][0];
+       newTiles[0][0] = newTiles[N - 1][N - 1];
+       newTiles[1][1] = temp;
+       return new Board(newTiles);
+   }
+
+   private static class ByHamming implements Comparator<Board>
+   {
+     public int compare(Board a, Board b)
+     {
+       int hamA = a.hamming() + a.moves;
+       int hamB = b.hamming() + b.moves;
+       if(hamA < hamB)
+       {
+         return -1;
+       }
+       else if(hamA > hamB)
+       {
+         return 1;
+       }
+       else
+       {
+         return 0;
+       }
+     }
+   }
+
+   private static class ByManhattan implements Comparator<Board>
+   {
+     public int compare(Board a, Board b)
+     {
+       int manA = a.manhattan();
+       int manB = b.manhattan();
+       if(manA < manB)
+       {
+         return -1;
+       }
+       else if(manA > manB)
+       {
+         return 1;
+       }
+       else
+       {
+         return 0;
+       }
+     }
    }
 
    //  test client
    public static void main(String[] args){
      int N = 3;
+     MinPQ priorityQueue = new MinPQ(4, BY_HAMMING);
      int[][] tiles = new int[N][N];
-//     for (int i = 0; i < N; i++)
-//         for (int j = 0; j < N; j++)
-//             tiles[i][j] = j + i*N + 1;
-     tiles[0][0] = 8;
-     tiles[0][1] = 1;
-     tiles[0][2] = 3;
-     tiles[1][0] = 4;
-     tiles[1][1] = 0;
-     tiles[1][2] = 2;
-     tiles[2][0] = 7;
-     tiles[2][1] = 6;
-     tiles[2][2] = 5;
-     System.out.println(tiles[0][2]);
-     Board board = new Board(tiles);
-     System.out.println(board.toString());
-     System.out.println(board.hamming());
-     System.out.println(board.manhattan());
-     Stack<Board> neighbors = (Stack<Board>) board.neighbors(); 
-     while(neighbors.size() > 0)
-     {
-         
-         System.out.println(neighbors.pop().toString());
-     }
-     
-     
-   }
+     for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            tiles[i][j] = j + i*N + 1;
+    tiles[0][0] = 8;
+    tiles[0][1] = 1;
+    tiles[0][2] = 3;
+    tiles[1][0] = 4;
+    tiles[1][1] = 0;
+    tiles[1][2] = 2;
+    tiles[2][0] = 7;
+    tiles[2][1] = 6;
+    tiles[2][2] = 5;
+    System.out.println(tiles[0][2]);
+    Board board = new Board(tiles);
+    Stack<Board> neighbors = (Stack<Board>) board.neighbors();
+    Board neighbor = neighbors.pop();
+    System.out.println(board.equals(neighbor));
+//    System.out.println(board.toString());
+//    System.out.println(board.hamming());
+//    System.out.println(board.manhattan());
+//    Stack<Board> neighbors = (Stack<Board>) board.neighbors();
+//    while(neighbors.size() > 0)
+//    {
+//        Board neighbor = neighbors.pop();
+//        priorityQueue.insert(neighbor);
+//    }
+//
+//    while(priorityQueue.size() > 0)
+//    {
+//        System.out.println(priorityQueue.delMin());
+//    }
+
+
+  }
 }
--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -1,23 +1,111 @@
+import java.util.Stack;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+
 public class Solver {
-   private Board initialBoard;
+   private int moves;
+   private int statesEnqueued;
+   private Queue<Board> boards;
+   private boolean solvable;
    public Solver(Board initial)   // find a solution to the initial board
    {
-       this.initialBoard = initial;
+       this.solvable = true;
+       this.boards = new Queue<Board>();
+       Queue<Board> altBoards = new Queue<Board>();
+       Board board = initial;
+       Board prevBoard = null;
+       Board altPrevBoard = null;
+       Board altboard = board.altBoard();
+       this.boards.enqueue(board);
+       altBoards.enqueue(altboard);
+       this.moves = 0;
+       this.statesEnqueued = 1;
+       MinPQ priorityQueue = new MinPQ(4, board.BY_HAMMING);
+       MinPQ altPriorityQueue = new MinPQ(4, board.BY_HAMMING);
+       while(board.hamming() > 0)
+       {
+           this.moves += 1;
+           Stack<Board> neighbors = (Stack<Board>) board.neighbors();
+           Stack<Board> altNeighbors = (Stack<Board>) altboard.neighbors();
+           while(neighbors.size() > 0)
+           {
+               Board neighbor = neighbors.pop();
+               
+               if(!neighbor.equals(prevBoard))
+               {
+                   this.statesEnqueued += 1;
+                   priorityQueue.insert(neighbor);
+               }
+               
+           }
+           while(altNeighbors.size() > 0)
+           {
+               Board neighbor = altNeighbors.pop();
+               
+               if(!neighbor.equals(altPrevBoard))
+               {
+                   altPriorityQueue.insert(neighbor);
+               }
+               
+           }
+           prevBoard = board;
+           altPrevBoard = altboard;
+           board = (Board) priorityQueue.delMin();
+           altboard = (Board) altPriorityQueue.delMin();
+           if(altboard.hamming() <= 0)
+           {
+               this.solvable = false;
+               break;
+           }
+           this.boards.enqueue(board);
+       }
    }
    public boolean isSolvable()    // is the initial board solvable?
+   {
+       return this.solvable;
+   }
+   public Queue<Board> solution()
+   {
+       return this.boards;
+   }
    public int moves()             // return min number of moves to solve the initial board;
-                                  // -1 if no such solution
+   {                              // -1 if no such solution
+       return this.moves;
+       
+   }
    public String toString()       // return string representation of solution (as described above)
+   {
+       String boardString = "";
+       if(isSolvable()){
+          while(this.boards.size() > 0)
+          {
+              boardString += this.boards.dequeue().toString();
+          }
+          boardString += "Number of states enqueued = " + this.statesEnqueued + "\n";
+          boardString += "Number of moves = " + this.moves();
+          return boardString; 
+       }
+       else
+       {
+           return "No solution possible";
+       }
+   }
 
    //  read puzzle instance from stdin and print solution to stdout (in format above)
    public static void main(String[] args)
    {
-     int N = StdIn.readInt();
+     String filename = args[0];
+     In in = new In(filename);
+     int N = in.readInt();
      int[][] tiles = new int[N][N];
+     
      for (int i = 0; i < N; i++)
          for (int j = 0; j < N; j++)
-             tiles[i][j] = StdIn.readInt();
+             tiles[i][j] = in.readInt();
      Board board = new Board(tiles);
      Solver solution = new Solver(board);
+     System.out.println(solution.toString());
    }
 }
